634283db50bd17748156b7c911b5c96f61397c49
Janine White <janine.white@gmail.com>
2019-11-17 13:15:44 -0700

    Got Mars Weather tweet

---
+++ config.py
@@ -0,0 +1,6 @@
+Twitter_App_ID = 17014691
+Twitter_Consumer_API_Key = "WQdCguuhZfRg0pYNdGpyLI0hN"
+Twitter_Consumer_Secret_API_Key = "Sqq79o2nyyJjNcnewMEqxIjg1ycUYcyiXMFFmds4PmvH1oog1Q"
+Twitter_Access_Token = "1238761-5bORR5xc8CDixWkQRaNGo36stB4ouQxhgS2e8dqQQs"
+Twitter_Access_Token_Secret = "l9ggiCpcKPK6EeQywPqV7cZm4ktZKMLZdPpV6tYS3GmMn"
+Twitter_Access_Level = "Read"
